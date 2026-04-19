bc2cedf4a5d498511f594fa533e2f104988074b4
cwcinc <cwc.inc.official@gmail.com>
2026-04-19 03:12:31 -0400

    babies first pml

--- 0.1.0/main.mod.js
+++ 0.1.0/main.mod.js
@@ -1,12 +1,27 @@
-import { PolyMod } from "https://cdn.polymodloader.com/cb/polytrackmods/PolyModLoader/0.6.0/PolyTypes.js";
+import { PolyMod, MixinType, SettingType } from "https://cdn.polymodloader.com/cb/polytrackmods/PolyModLoader/0.6.0/PolyTypes.js";
 
 class nightMod extends PolyMod {
     preInit = (pml) => {
-        // pre init
+        /**
+         *                     (0,
+                    i.gn)(this, T, "f").add(new r.dth(3891597,11714755,4.7)),
+                    document.addEventListener("fullscreenchange", ( () => {
+                        (0,
+                        i.gn)(this, y, "m", F).call(this)
+                    }
+                    )),
+         */
+        pml.registerGlobalMixin({
+            type: MixinType.REPLACEBETWEEN,
+            tokenStart: `i.gn)(this, T, "f").add(new r.dth(3891597, 11714755, 4.7))`,
+            tokenEnd: `i.gn)(this, T, "f").add(new r.dth(3891597, 11714755, 4.7))`,
+            func: `i.gn)(this, T, "f").add(new r.dth(3891597, 11714755, window.polyModLoader.getSetting("Nighttime") == "false" ? 4.7 : -10))`,
+        });
     }
     init = (pml) => {
         this.pml = pml; // so pml is accessible outside of init (not neccesary)
-        // regular init
+        pml.registerSettingCategory("Night Mod");
+        pml.registerSetting("Nighttime", "Nighttime", SettingType.BOOL, true);
     }
     postInit = () => {
         // post init
