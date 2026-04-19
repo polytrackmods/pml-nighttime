17a8274c71cde37bad95cec571842a58ae0f7f8c
Cameron Zifcak <137846994+cwcinc@users.noreply.github.com>
2026-04-19 02:00:05 -0400

    Initial commit

---
+++ 0.1.0/main.mod.js
@@ -0,0 +1,19 @@
+import { PolyMod } from "https://cdn.polymodloader.com/cb/polytrackmods/PolyModLoader/0.6.0/PolyTypes.js";
+
+class templateMod extends PolyMod {
+    preInit = (pml) => {
+        // pre init
+    }
+    init = (pml) => {
+        this.pml = pml; // so pml is accessible outside of init (not neccesary)
+        // regular init
+    }
+    postInit = () => {
+        // post init
+    }
+    simInit = () => {
+        // sim init here
+    } 
+}
+
+export let polyMod = new templateMod();
